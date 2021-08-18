f6a955b6de66839cac8af223a95c94856432e75e
moedevo <alzubaidimustafabusiness@gmail.com>
2021-08-18 17:44:37 +0300

    Initial commit

---
+++ JavaScript/objects.js
@@ -0,0 +1,23 @@
+// 1. Create an array which contains an object and name the array "database"
+var database = [
+	{
+		username: "andrei",
+		password: "supersecret"
+	}
+];
+
+// 2. Create an array called "newsfeed" which contains 3 objects with properties "username" and "timeline".
+var newsfeed = [
+	{
+		username: "Bobby",
+		timeline: "So tired from all that learning!"
+	},
+	{
+		username: "Sally",
+		timeline: "Javascript is sooooo cool!"
+	},
+	{
+		username: "Mitch",
+		timeline: "Javascript is preeetyy cool!"
+	}
+];
